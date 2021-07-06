4adf5d143bcdf6daf84b631a0c16b06cc89e1ff6
Andre Menezes <ios.andrem@gmail.com>
2021-07-06 19:16:23 +0100

    Add new empty project to git

---
+++ SwiftUITeste/SwiftUITeste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITeste
+//
+//  Created by Andre Pessoal on 06/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
