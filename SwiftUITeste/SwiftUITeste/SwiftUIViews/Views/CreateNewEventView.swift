7cc1d1012af3c74ae82d06cd2ede3bc16b7d0f59
Andre Menezes <ios.andrem@gmail.com>
2023-08-01 14:18:54 -0300

    Add a alert to avoid events to be save without a name, and add a class to preparation of apple's EventKit

--- SwiftUITeste/SwiftUITeste/SwiftUIViews/Views/CreateNewEventView.swift
+++ SwiftUITeste/SwiftUITeste/SwiftUIViews/Views/CreateNewEventView.swift
@@ -8,27 +8,28 @@
 import SwiftUI
 
 struct CreateNewEventView: View {
-
+    
     @Environment(\.presentationMode) var presentationMode
     @StateObject var event: EventEntity = EventEntity()
     @State var showTimeOfEvent: Bool = false
     @State var showImagePicker: Bool = false
-
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationView {
-
+            
             Form {
-
+                
                 Section {
                     EventLabelView(title: "Nome do Evento",
                                    image: Image(systemName: "keyboard"),
                                    backgroundColor: .green)
-
+                    
                     TextField("Nome do Evento",
                               text: $event.title)
-                        .autocapitalization(.words)
+                    .autocapitalization(.words)
                 }
-
+                
                 Section {
                     EventLabelView(title: "Data do Evento",
                                    image: Image(systemName: "calendar"),
@@ -37,8 +38,7 @@ struct CreateNewEventView: View {
                     DatePicker("Data do Evento",
                                selection: $event.date,
                                displayedComponents: self.datePickerComponentsToShow())
-                    .frame(maxHeight: 300)
-                    .frame(minHeight: 300)
+                    .frame(height: 340)
                     .datePickerStyle(GraphicalDatePickerStyle())
                     .padding()
                     
@@ -48,17 +48,17 @@ struct CreateNewEventView: View {
                                        backgroundColor: .blue)
                     }
                 }
-
+                
                 Section {
-
+                    
                     if let image = self.event.image() {
                         HStack {
                             EventLabelView(title: "",
                                            image: Image(systemName: "camera"),
                                            backgroundColor: .purple)
-
+                            
                             Spacer()
-
+                            
                             Button(action: {
                                 self.event.imageData = nil
                                 self.showImagePicker = false
@@ -68,14 +68,14 @@ struct CreateNewEventView: View {
                             }
                             .buttonStyle(BorderlessButtonStyle()) //Fix a swiftUI bug that when a button was touched they triger other buttons on the same section
                         }
-
+                        
                         Button(action: {
                             self.showImagePicker = true
                         }) {
                             image
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-
+                            
                         }
                         .buttonStyle(BorderlessButtonStyle()) //Fix a swiftUI bug that when a button was touched they triger other buttons on the same section
                     } else {
@@ -91,11 +91,11 @@ struct CreateNewEventView: View {
                             }
                         }
                     }
-
+                    
                 }.sheet(isPresented: $showImagePicker) {
                     ImagePickerUISwiftHelper(imageDate: $event.imageData)
                 }
-
+                
                 Section {
                     ColorPicker(selection: $event.color) {
                         EventLabelView(title: "Cor da categoria do evento",
@@ -111,7 +111,7 @@ struct CreateNewEventView: View {
                     TextField("Adicione um endereço para o seu evento", text: $event.address)
                         .autocapitalization(.none)
                 }
-
+                
                 Section {
                     EventLabelView(title: "URL",
                                    image: Image(systemName: "link"),
@@ -128,16 +128,29 @@ struct CreateNewEventView: View {
                 Text("Cancelar")
                     .font(.title2)
             }, trailing: Button(action: {
-                DataController.shared.saveEvent(event: self.event)
-                self.presentationMode.wrappedValue.dismiss()
+                self.checkIfCanBeSave()
             }) {
                 Text("Salvar")
                     .font(.title2)
                     .bold()
             })
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Erro ao salvar seu evento"),
+                      message: Text("O Evento precisa ter um Nome"),
+                      dismissButton: .default(Text("Ok!")))
+            }
         }
-
     }
+    
+    func checkIfCanBeSave() {
+        if self.event.title.isEmpty {
+            showingAlert = true
+        } else {
+            DataController.shared.saveEvent(event: self.event)
+            self.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
 }
 
 extension CreateNewEventView {
@@ -151,6 +164,6 @@ struct CreateNewEvent_Previews: PreviewProvider {
         NavigationView {
             CreateNewEventView()
         }
-
+        
     }
 }
